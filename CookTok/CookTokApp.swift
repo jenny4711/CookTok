8d9b597b44741fb4bd512d83d5c06bfefdc2ebb6
jiylee <jjenny4711@gmail.com>
2025-07-16 02:22:32 -0400

    Initial Commit

---
+++ CookTok/CookTokApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookTokApp.swift
+//  CookTok
+//
+//  Created by Ji y LEE on 7/16/25.
+//
+
+import SwiftUI
+
+@main
+struct CookTokApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
