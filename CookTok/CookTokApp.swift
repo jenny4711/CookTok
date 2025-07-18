403e30889465a2f5aac1aaf2f18a1ba0d03e7eac
jiylee <jjenny4711@gmail.com>
2025-07-17 23:52:09 -0400

    add instruction

--- CookTok/CookTokApp.swift
+++ CookTok/CookTokApp.swift
@@ -17,3 +17,31 @@ struct CookTokApp: App {
         }
     }
 }
+// instruction
+
+//About
+//The CookTokApp helps users manage their groceries. When you're food shopping, you can create a list. Once you've purchased items, clicking on them in the shopping list automatically saves them to your refrigerator inventory. This refrigerator list displays all the ingredients currently in your fridge. To help you keep track, items whose expiration date has passed will automatically turn red.
+
+
+
+//Introduce Pages
+//OnboardingPage
+//Tapping the "Open" button navigates to the next section.
+
+
+//ShoppingListPage
+//In the first part, filling out a form and pressing "Save" displays the item in a list in the second part.
+
+//Tapping the refrigerator button on the top right navigates to the "ItemListPage."
+
+//Clicking on a purchased item deletes it from the shopping list and moves it to the "ItemListPage."
+
+
+//ItemListPage
+//This page displays a list of items currently in the refrigerator.
+
+//Selecting a desired category from the "Category" section shows a list of items related to that category.
+
+//Clicking on an item in the list brings up a form to modify its expiration date and category, along with a "Delete" button.
+
+
